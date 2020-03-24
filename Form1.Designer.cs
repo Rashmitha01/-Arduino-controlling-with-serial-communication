a2f7cc8ae49b659bd90e435815ca20e0e1d73ed3
Rashmitha <36674921+Rashmitha01@users.noreply.github.com>
2020-03-25 03:20:09 +0530

    initial version is added
    initial version contains only read and save data to a file

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace recieve_save_data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.value_tb = new System.Windows.Forms.TextBox();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.save_data_btn = new System.Windows.Forms.Button();
+            this.port_tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.value_pb = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // value_tb
+            // 
+            this.value_tb.Location = new System.Drawing.Point(30, 31);
+            this.value_tb.Multiline = true;
+            this.value_tb.Name = "value_tb";
+            this.value_tb.Size = new System.Drawing.Size(235, 361);
+            this.value_tb.TabIndex = 0;
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(399, 211);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(138, 47);
+            this.start_btn.TabIndex = 1;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(399, 276);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(138, 47);
+            this.stop_btn.TabIndex = 2;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // save_data_btn
+            // 
+            this.save_data_btn.Location = new System.Drawing.Point(399, 343);
+            this.save_data_btn.Name = "save_data_btn";
+            this.save_data_btn.Size = new System.Drawing.Size(138, 47);
+            this.save_data_btn.TabIndex = 3;
+            this.save_data_btn.Text = "Save Data";
+            this.save_data_btn.UseVisualStyleBackColor = true;
+            this.save_data_btn.Click += new System.EventHandler(this.datasave_btn_Click);
+            // 
+            // port_tb
+            // 
+            this.port_tb.Location = new System.Drawing.Point(399, 45);
+            this.port_tb.Name = "port_tb";
+            this.port_tb.Size = new System.Drawing.Size(185, 22);
+            this.port_tb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PORT NAME";
+            // 
+            // value_pb
+            // 
+            this.value_pb.Location = new System.Drawing.Point(30, 398);
+            this.value_pb.Name = "value_pb";
+            this.value_pb.Size = new System.Drawing.Size(235, 23);
+            this.value_pb.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 424);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 424);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "1024";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.value_pb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.port_tb);
+            this.Controls.Add(this.save_data_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.value_tb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox value_tb;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button save_data_btn;
+        private System.Windows.Forms.TextBox port_tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar value_pb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace recieve_save_data
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort myport;
+        private DateTime datetime;
+        private string in_data;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            //myport.WriteLine("start");
+            myport.DataReceived += Myport_DataReceived;
+
+        }
+
+        private void Myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(displaydata_event));
+             in_data = myport.ReadLine();           
+        }
+
+        private void displaydata_event(object sender, EventArgs e)
+        {
+            datetime = DateTime.Now;
+            string time = datetime.Hour + ":" + datetime.Minute + ":" + datetime.Second;
+            value_tb.AppendText(time + "\t\t\t\t" + in_data + "\n");
+            //status of data in progress bar 
+            int data_value = Convert.ToInt32(in_data);
+            value_pb.Value = data_value;
+        }
+
+        private void Init()
+        {
+            try
+            {
+                myport = new SerialPort();
+                myport.BaudRate = 9600;
+                myport.PortName = port_tb.Text;
+                myport.Parity = Parity.None;
+                myport.DataBits = 8;
+                myport.StopBits = StopBits.One;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "please fill all the fields");
+            }
+            
+            try
+            {
+                myport.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "COM Port error!!!Error opening port");
+            }
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myport.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "COM port error!! cannot close the port");
+            }
+        }
+
+        private void datasave_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filepath = @"C:\Users\User\Desktop\DATA";
+                string filename = "serial_data";
+                System.IO.File.WriteAllText(filepath + filename, value_tb.Text);
+                MessageBox.Show("serial data is being saved to " + filepath, "Save File");
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error while saving data to file");
+            }
+            
+      
+        }
+    }
+}
